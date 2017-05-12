6da9ed610089c5b2a6625ce176ae8eb84d00dbaf
Ultime5528 <keranna.first@gmail.com>
2017-05-12 17:13:08 -0400

    Ajout des solutions section 1

---
+++ code/Solutions/1-1-Presentation.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+/**
+ * Exercice 1.1
+ * 
+ * Demande des informations sur l'utilisateur puis les affiche.
+ * 
+ * @author Etienne
+ *
+ */
+public class Presentation {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		//Renseignements de l'utilisateur
+		String nom;
+		int age;
+		double salaire;
+		
+		
+		//Demande les informations
+		System.out.print("Saisissez votre nom : ");
+		nom = scanner.nextLine();
+		
+		System.out.print("Saisissez votre âge : ");
+		age = scanner.nextInt();
+		
+		System.out.print("Saisissez votre salaire : ");
+		salaire = scanner.nextDouble();
+		
+		
+		//Affichage des informations
+		System.out.println("\nBonjour " + nom + "! Vous avez " + age
+				+ " ans et votre salaire est de " + salaire + " $ par heure.");
+
+	}
+
+}
---
+++ code/Solutions/1-2-AireRectangle.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+/**
+ * Exercice 1.2
+ * 
+ * Calcul de l'aire d'un rectangle.
+ * 
+ * @author Etienne
+ *
+ */
+public class AireRectangle {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		double hauteur, largeur, aire;
+		
+		
+		//Données de l'utilisateur
+		System.out.print("Hauteur du rectangle : ");
+		hauteur = scanner.nextDouble();
+		
+		System.out.print("Largeur du rectangle : ");
+		largeur = scanner.nextDouble();
+		
+		
+		//Calcul de l'aire
+		aire = hauteur * largeur;
+		
+		
+		//Affichage de l'aire
+		System.out.println("\nLe rectangle a une aire de " + aire + " unités carrées.");
+
+	}
+
+}
